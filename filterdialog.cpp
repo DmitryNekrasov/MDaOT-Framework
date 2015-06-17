646c98b41f25f523242690e9850aa9a31486704e
DmitryNekrasov <scannorone@gmail.com>
2015-06-17 23:19:29 +0600

    + возможность редактировать фильтры

--- filterdialog.cpp
+++ filterdialog.cpp
@@ -23,6 +23,72 @@ void FilterDialog::setFilter(Filter *filter)
     fh->setWindow(this);
     filter->setFilterHandler(fh);
     this->setWindowTitle(filter->getName());
+
+    if (dynamic_cast<BinaryFilter*>(filter)) {
+        filterCode = BINARY_CODE;
+        binaryFilter = dynamic_cast<BinaryFilter*>(filter);
+    } else if (dynamic_cast<BlurFilter*>(filter)) {
+        filterCode = BLUR_CODE;
+        blurFilter = dynamic_cast<BlurFilter*>(filter);
+    } else if (dynamic_cast<MedianFilter*>(filter)) {
+        filterCode = MEDIAN_CODE;
+        medianFilter = dynamic_cast<MedianFilter*>(filter);
+    } else if (dynamic_cast<GrayscaleFilter*>(filter)) {
+        filterCode = GRAYSCALE_CODE;
+        grayscaleFilter = dynamic_cast<GrayscaleFilter*>(filter);
+    }
+
+    nextProcessing();
+}
+
+void FilterDialog::nextProcessing()
+{
+    if (filterCode == BINARY_CODE) {
+        ui->horizontalSlider->setValue(binaryFilter->getThreshold());
+        makeFalseBlurSettings();
+    } else if (filterCode == MEDIAN_CODE) {
+
+        ui->horizontalSlider->setValue(medianFilter->getSize());
+
+        makeFalseBlurSettings();
+
+        ui->label->setText("Размер:");
+
+        ui->horizontalSlider->setMinimum(MINIMUM_MEDIAN);
+        ui->horizontalSlider->setMaximum(MAXIMUM_MEDIAN);
+        ui->horizontalSlider->setSingleStep(2);
+
+        ui->spinBox->setMinimum(MINIMUM_MEDIAN);
+        ui->spinBox->setMaximum(MAXIMUM_MEDIAN);
+        ui->spinBox->setSingleStep(2);
+
+    } else if (filterCode == BLUR_CODE) {
+
+        ui->horizontalSlider->setValue(blurFilter->getWidthBlur());
+        ui->horizontalSliderBlur->setValue(blurFilter->getHeightBlur());
+
+        ui->label->setText("Ширина:");
+
+        ui->horizontalSlider->setMinimum(MINIMUM_BLUR);
+        ui->horizontalSlider->setMaximum(MAXIMUM_BLUR);
+
+        ui->spinBox->setMinimum(MINIMUM_BLUR);
+        ui->spinBox->setMaximum(MAXIMUM_BLUR);
+
+        ui->horizontalSliderBlur->setMinimum(MINIMUM_BLUR);
+        ui->horizontalSliderBlur->setMaximum(MAXIMUM_BLUR);
+
+        ui->spinBoxBlur->setMinimum(MINIMUM_BLUR);
+        ui->spinBoxBlur->setMaximum(MAXIMUM_BLUR);
+
+    }
+}
+
+void FilterDialog::makeFalseBlurSettings()
+{
+    ui->labelBlur->setVisible(false);
+    ui->spinBoxBlur->setVisible(false);
+    ui->horizontalSliderBlur->setVisible(false);
 }
 
 void FilterDialog::closeEvent(QCloseEvent *event)
@@ -47,8 +113,31 @@ void FilterDialog::paintEvent(QPaintEvent *)
 void FilterDialog::on_closeButton_clicked()
 {
     this->close();
-
 }
 
 void FilterDialog::on_FilterDialog_destroyed()
 {}
+
+
+
+void FilterDialog::on_horizontalSlider_valueChanged(int value)
+{
+    if (filterCode == BINARY_CODE) {
+        binaryFilter->setThreshold(value);
+    } else if (filterCode == MEDIAN_CODE) {
+        if (value % 2 == 0) {
+            value++;
+            ui->horizontalSlider->setValue(value);
+        }
+        medianFilter->setSize(value);
+    } else if (filterCode == BLUR_CODE) {
+        blurFilter->setWidthBlur(value);
+    }
+}
+
+void FilterDialog::on_horizontalSliderBlur_valueChanged(int value)
+{
+    if (filterCode == BLUR_CODE) {
+        blurFilter->setHeightBlur(value);
+    }
+}
--- filterdialog.h
+++ filterdialog.h
@@ -1,6 +1,16 @@
 #ifndef FILTERDIALOG_H
 #define FILTERDIALOG_H
 
+#define BINARY_CODE 1
+#define BLUR_CODE 2
+#define MEDIAN_CODE 3
+#define GRAYSCALE_CODE 4
+
+#define MINIMUM_MEDIAN 1
+#define MAXIMUM_MEDIAN 15
+#define MINIMUM_BLUR 1
+#define MAXIMUM_BLUR 25
+
 #include <QDialog>
 #include <QDebug>
 #include <QImage>
@@ -8,6 +18,10 @@
 #include "filter.h"
 #include "filterhandler.h"
 #include "myfilterhandler.h"
+#include "binaryfilter.h"
+#include "blurfilter.h"
+#include "medianfilter.h"
+#include "grayscalefilter.h"
 
 namespace Ui {
 class FilterDialog;
@@ -34,11 +48,23 @@ private slots:
 
     void on_FilterDialog_destroyed();
 
+    void on_horizontalSlider_valueChanged(int value);
+
+    void on_horizontalSliderBlur_valueChanged(int value);
+
 private:
     Ui::FilterDialog *ui;
     Filter *filter;
+    BinaryFilter *binaryFilter;
+    BlurFilter *blurFilter;
+    MedianFilter *medianFilter;
+    GrayscaleFilter *grayscaleFilter;
+    int filterCode;
 
     QImage *qimg;
+
+    void nextProcessing();
+    void makeFalseBlurSettings();
 
 signals:
     void repaintSignal();
