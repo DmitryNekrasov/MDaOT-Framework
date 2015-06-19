21351f43657967f1e071c7497256b59360aa4784
DmitryNekrasov <scannorone@gmail.com>
2015-06-19 23:16:44 +0600

    + документация класса MyFilterHandler

--- myfilterhandler.h
+++ myfilterhandler.h
@@ -3,13 +3,32 @@
 #include "filterhandler.h"
 
 
+/*!
+ * Реализация обработчика фильтра.
+ * Отдаёт кадр после применения фильтра на форму.
+ * \brief MyFilterHandler class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class MyFilterHandler: public FilterHandler
 {
 public:
 
+    /*!
+     * Выполняет действия с кадром после применения фильтра.
+     * Отдаёт кадр после применения фильтра на форму.
+     * \brief onApplyFilter
+     * \param frame Кадр
+     */
     virtual void onApplyFilter(Frame frame);
 
+    /*!
+     * Конструктор по умолчанию
+     * \brief MyFilterHandler
+     */
     MyFilterHandler();
+
     ~MyFilterHandler();
 };
 
