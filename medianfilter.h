d1445336090e44cf6fdb9861804f07f9091165ab
DmitryNekrasov <scannorone@gmail.com>
2015-06-19 22:41:39 +0600

    + документация класса MedianFilter

--- medianfilter.h
+++ medianfilter.h
@@ -4,6 +4,13 @@
 #include "filterhandler.h"
 
 
+/*!
+ * Медианный фильтр
+ * \brief MedianFilter class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class MedianFilter: public Filter
 {
 private:
@@ -12,13 +19,43 @@ private:
 
 public:
 
+    /*!
+     * Применяет фильтр к кадру
+     * \brief apply
+     * \param originalFrame Кадр
+     * \return Кадр, полученный после применения фильтра
+     */
     virtual Frame apply(Frame originalFrame);
 
+    /*!
+     * Возвращает размер медианного фильтра
+     * \brief getSize
+     * \return Размер медианного фильтра
+     */
     int getSize();
+
+    /*!
+     * Изменяет размер медианного фильтра
+     * \brief setSize
+     * \param size Размер медианного фильтра
+     */
     void setSize(int size);
 
+    /*!
+     * Конструктор
+     * \brief MedianFilter
+     * \param size Размер медианного фильтра
+     */
     MedianFilter(int size);
+
+    /*!
+     * Конструктор
+     * \brief MedianFilter
+     * \param size Размер медианного фильтра
+     * \param handler Обработчик фильтра
+     */
     MedianFilter(int size, FilterHandler *handler);
+
     ~MedianFilter();
 };
 
