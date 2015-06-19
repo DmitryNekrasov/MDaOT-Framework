6e2e1c04d6753b6aa7dae5110638655ed801b02f
DmitryNekrasov <scannorone@gmail.com>
2015-06-19 22:32:34 +0600

    + документация класса BlurFilter

--- blurfilter.h
+++ blurfilter.h
@@ -4,6 +4,13 @@
 #include "filterhandler.h"
 
 
+/*!
+ * Фильтр размытие
+ * \brief BlurFilter class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class BlurFilter: public Filter
 {
 private:
@@ -13,16 +20,59 @@ private:
 
 public:
 
+    /*!
+     * Применяет фильтр к кадру
+     * \brief apply
+     * \param originalFrame Кадр
+     * \return Кадр, полученный после применения фильтра
+     */
     virtual Frame apply(Frame originalFrame);
 
+    /*!
+     * Возвращает ширину матрицы свёртки
+     * \brief getWidthBlur
+     * \return Ширина матрицы свёртки
+     */
     int getWidthBlur();
+
+    /*!
+     * Изменяет ширину матрицы свёртки
+     * \brief setWidthBlur
+     * \param widthBlur Ширина матрицы свёртки
+     */
     void setWidthBlur(int widthBlur);
 
+    /*!
+     * Возвращает высоту матрицы свёртки
+     * \brief getHeightBlur
+     * \return Высота матрицы свёртки
+     */
     int getHeightBlur();
+
+    /*!
+     * Изменяет высоту матрицы свёртки
+     * \brief setHeightBlur
+     * \param heightBlur Высота матрицы свёртки
+     */
     void setHeightBlur(int heightBlur);
 
+    /*!
+     * Конструктор
+     * \brief BlurFilter
+     * \param widthBlur Ширина матрицы свёртки
+     * \param heightBlur Высота матрицы свёртки
+     */
     BlurFilter(int widthBlur, int heightBlur);
+
+    /*!
+     * Конструктор
+     * \brief BlurFilter
+     * \param widthBlur Ширина матрицы свёртки
+     * \param heightBlur Высота матрицы свёртки
+     * \param handler Обработчик фильтра
+     */
     BlurFilter(int widthBlur, int heightBlur, FilterHandler *handler);
+
     ~BlurFilter();
 };
 
