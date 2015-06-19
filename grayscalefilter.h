26934e7edf9b5e9363342885976402f97e1db961
DmitryNekrasov <scannorone@gmail.com>
2015-06-19 22:18:24 +0600

    + документация класса GrayscaleFilter

--- grayscalefilter.h
+++ grayscalefilter.h
@@ -4,14 +4,38 @@
 #include "filterhandler.h"
 
 
+/*!
+ * Фильтр градации серого
+ * \brief GrayscaleFilter class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class GrayscaleFilter: public Filter
 {
 public:
 
+    /*!
+     * Применяет фильтр к кадру
+     * \brief apply
+     * \param originalFrame Кадр
+     * \return Кадр, полученный после применения фильтра
+     */
     virtual Frame apply(Frame originalFrame);
 
+    /*!
+     * Конструктор по умолчанию
+     * \brief GrayscaleFilter
+     */
     GrayscaleFilter();
+
+    /*!
+     * Конструктор
+     * \brief GrayscaleFilter
+     * \param handler Обработчик фильтра
+     */
     GrayscaleFilter(FilterHandler *handler);
+
     ~GrayscaleFilter();
 };
 
