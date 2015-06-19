69561951ae7df3026c8725f3ca58c7015909a986
DmitryNekrasov <scannorone@gmail.com>
2015-06-19 23:53:03 +0600

    + документация класса Rectangle

--- rectangle.h
+++ rectangle.h
@@ -6,6 +6,15 @@
 #include <opencv2/imgproc/imgproc_c.h>
 #include <opencv2/opencv.hpp>
 
+
+/*!
+ * Прямоугольник.
+ * Используется для выделения обнаруженных объектов.
+ * \brief Rectangle class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class Rectangle
 {
 
@@ -13,11 +22,37 @@ class Rectangle
 
 public:
 
+    /*!
+     * Возвращает объект класса cv::Rect
+     * \brief getCvRect
+     * \return Объект класса cv::Rect
+     */
     cv::Rect getCvRect();
+
+    /*!
+     * Возвращает площадь прямоугольника
+     * \brief getArea
+     * \return Площадь прямоугольника
+     */
     int getArea();
 
+    /*!
+     * Конструктор
+     * \brief Rectangle
+     * \param x Координата x верхнего левого угла прямоугольника
+     * \param y Координата y верхнего левого угла прямоугольника
+     * \param width Ширина прямоугольника
+     * \param height Высота прямоугольника
+     */
     Rectangle(int x, int y, int width, int height);
+
+    /*!
+     * Конструктор
+     * \brief Rectangle
+     * \param cvRect Объект класса cv::Rect
+     */
     Rectangle(cv::Rect cvRect);
+
     ~Rectangle();
 };
 
