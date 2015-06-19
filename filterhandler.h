a1042c0a260d493b4a4151d42704ee9b1876ae12
DmitryNekrasov <scannorone@gmail.com>
2015-06-19 22:53:12 +0600

    + документация класса FilterHandler

--- filterhandler.h
+++ filterhandler.h
@@ -4,14 +4,34 @@
 
 class FilterDialog;
 
+
+/*!
+ * Абстрактный класс обработчика фильтра
+ * \brief FilterHandler class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class FilterHandler
 {
 protected:
+
     FilterDialog *window;
 
 public:
 
+    /*!
+     * Выполняет действия с кадром после применения фильтра
+     * \brief onApplyFilter
+     * \param frame Кадр
+     */
     virtual void onApplyFilter(Frame frame) = 0;
+
+    /*!
+     * Изменяет окно, на которое можно отдавать отфильтрованное изображение
+     * \brief setWindow
+     * \param win Окно
+     */
     void setWindow(FilterDialog *win);
 
     ~FilterHandler();
