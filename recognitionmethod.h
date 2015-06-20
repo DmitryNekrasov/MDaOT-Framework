5289c0ba0c0f7064f1bbabc02993ed4e1aca1e00
DmitryNekrasov <scannorone@gmail.com>
2015-06-20 11:55:10 +0600

    + документация класса RecognitionMethod

--- recognitionmethod.h
+++ recognitionmethod.h
@@ -3,10 +3,23 @@
 #include "frame.h"
 
 
+/*!
+ * Абстрактный класс стратегии.
+ * Метод распознавания объектов.
+ * \brief The RecognitionMethod class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class RecognitionMethod
 {   
 public:
 
+    /*!
+     * Распознаёт объекты на кадре
+     * \brief recognize
+     * \param frame Кадр
+     */
     virtual void recognize(Frame frame) = 0;
 
     ~RecognitionMethod();
