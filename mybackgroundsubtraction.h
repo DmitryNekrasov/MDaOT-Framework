0386e8c9bc7a0577565ef0b6d5cb821d61747ea3
DmitryNekrasov <scannorone@gmail.com>
2015-06-20 10:56:44 +0600

    + документация класса MyBackgroundSubtraction

--- mybackgroundsubtraction.h
+++ mybackgroundsubtraction.h
@@ -4,19 +4,50 @@
 #include "detectionmethod.h"
 
 
+/*!
+ * Реализация метода определения движения.
+ * Метод вычитания фона.
+ * \brief The MyBackgroundSubtraction class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class MyBackgroundSubtraction: public DetectionMethod
 {
 private:
+
     Frame background;
     Filter *grayscaleFilter;
 
 public:
 
+    /*!
+     * Находит движущиеся объекты на видео
+     * \brief detect
+     * \param video Видео
+     */
     virtual void detect(Video video);
+
+    /*!
+     * Инициализирует метод вычитания фона (цепочка фильтров, фон)
+     * \brief init
+     * \param video Видео
+     */
     virtual void init(Video video);
 
+    /*!
+     * Конструктор по умолчанию
+     * \brief MyBackgroundSubtraction
+     */
     MyBackgroundSubtraction();
+
+    /*!
+     * Конструктор
+     * \brief MyBackgroundSubtraction
+     * \param handler Обработчик движения
+     */
     MyBackgroundSubtraction(MovenmentHandler *handler);
+
     ~MyBackgroundSubtraction();
 };
 
