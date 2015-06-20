c09d9daa8056fa8485a310290c022045cccb628e
DmitryNekrasov <scannorone@gmail.com>
2015-06-20 11:58:23 +0600

    + документация класса Recognizer

--- recognizer.h
+++ recognizer.h
@@ -4,17 +4,41 @@
 #include "recognitionmethod.h"
 
 
+/*!
+ * Контекст стратегии распознавания объектов.
+ * \brief The Recognizer class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class Recognizer
 {
 private:
+
     RecognitionMethod *recognitionMethod;
 
 public:
 
+    /*!
+     * Изменяет метод распознавания объектов
+     * \brief setRecognitionMethod
+     * \param recognitionMethod Метод распознавания объектов
+     */
     void setRecognitionMethod(RecognitionMethod *recognitionMethod);
+
+    /*!
+     * Выполняет распознавание объектов на кадре заданным методом
+     * \brief execute
+     * \param frame Кадр
+     */
     void execute(Frame frame);
 
+    /*!
+     * Конструктор по умолчанию
+     * \brief Recognizer
+     */
     Recognizer();
+
     ~Recognizer();
 };
 
