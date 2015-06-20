54f865526942b32a602950134bfcad3695406ce4
DmitryNekrasov <scannorone@gmail.com>
2015-06-20 11:51:19 +0600

    + документация класса MovenmentHandler

--- movenmenthandler.h
+++ movenmenthandler.h
@@ -4,14 +4,36 @@
 
 class MainWindow;
 
+
+/*!
+ * Абстрактный класс обработчика движения
+ * \brief MovenmentHandler class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class MovenmentHandler
 {
 protected:
+
     MainWindow *window;
 
 public:
 
+    /*!
+     * Выполняет действия с кадром после обнаружения движения
+     * \brief onMove
+     * \param frame Кадр
+     * \param rectangles Прямоугольники, ограничивающие обнаруженные объекты
+     * \param mask Контуры движущихся объектов
+     */
     virtual void onMove(Frame frame, vector<Rectangle> *rectangles, vector<Point> *mask) = 0;
+
+    /*!
+     * Изменяет окно, на которое можно отдавать кадр с обнаруженными объектами
+     * \brief setWindow
+     * \param win Окно
+     */
     void setWindow(MainWindow *win);
 
     ~MovenmentHandler();
