14bf7694cb3a2a6b07bab4f375ce1e799566b974
DmitryNekrasov <scannorone@gmail.com>
2015-06-20 10:52:35 +0600

    + документация класса SequentialImages

--- sequentialimages.h
+++ sequentialimages.h
@@ -3,6 +3,14 @@
 #include "detectionmethod.h"
 
 
+/*!
+ * Реализация метода определения движения.
+ * Метод последовательных изображений.
+ * \brief The SequentialImages class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class SequentialImages: public DetectionMethod
 {
 private:
@@ -12,11 +20,33 @@ private:
 
 public:
 
+    /*!
+     * Находит движущиеся объекты на видео
+     * \brief detect
+     * \param video Видео
+     */
     virtual void detect(Video video);
+
+    /*!
+     * Инициализирует метод (цепочка фильтров)
+     * \brief init
+     * \param video Видео
+     */
     virtual void init(Video video);
 
+    /*!
+     * Конструктор по умолчанию
+     * \brief SequentialImages
+     */
     SequentialImages();
+
+    /*!
+     * Конструктор
+     * \brief SequentialImages
+     * \param handler Обработчик двидения
+     */
     SequentialImages(MovenmentHandler *handler);
+
     ~SequentialImages();
 };
 
