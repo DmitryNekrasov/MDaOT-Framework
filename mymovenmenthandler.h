eec1b86dcb889220a0d42a275a7cbcbd4dfa46ce
DmitryNekrasov <scannorone@gmail.com>
2015-06-20 11:53:05 +0600

    + документация класса MyMovenmentHandler

--- mymovenmenthandler.h
+++ mymovenmenthandler.h
@@ -3,13 +3,32 @@
 #include "movenmenthandler.h"
 
 
+/*!
+ * Реализация обработчика движения
+ * \brief MyMovenmentHandler class
+ * \author Dmitry Nekrasov
+ * \version 0.1
+ * \date Июнь 2015
+ */
 class MyMovenmentHandler: public MovenmentHandler
 {
 public:
 
+    /*!
+     * Выполняет действия с кадром после обнаружения движения
+     * \brief onMove
+     * \param frame Кадр
+     * \param rectangles Прямоугольники, ограничивающие обнаруженные объекты
+     * \param mask Контуры движущихся объектов
+     */
     virtual void onMove(Frame frame, vector<Rectangle> *rectangles, vector<Point> *mask);
 
+    /*!
+     * Конструктор по умолчанию
+     * \brief MyMovenmentHandler
+     */
     MyMovenmentHandler();
+
     ~MyMovenmentHandler();
 };
 
